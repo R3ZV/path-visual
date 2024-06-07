63e52ae6de9d68a10c5fb9e0035c3842295ab2a2
R3Zv <razvan.maracine@protonmail.com>
2024-06-07 22:30:02 +0300

    Better server message

--- app.js
+++ app.js
@@ -14,86 +14,86 @@ app.use(express.static(`${__dirname}/assets`));
 app.use(express.static(`${__dirname}/scripts`));
 
 app.use(
-    session({
-        secret: "salty",
-        resave: true,
-        saveUninitialized: false,
-    }),
+	session({
+		secret: "salty",
+		resave: true,
+		saveUninitialized: false,
+	}),
 );
 
 app.get("/", (req, res) => {
-    if (req.session.loggedin === undefined) {
-        req.session.loggedin = false;
-    }
-    res.render("index", { loggedin: req.session.loggedin });
+	if (req.session.loggedin === undefined) {
+		req.session.loggedin = false;
+	}
+	res.render("index", { loggedin: req.session.loggedin });
 });
 
 app.get("/login", (req, res) => {
-    if (req.session.loggedin === true) {
-        res.redirect("/");
-        return;
-    }
-    res.render("login", { loggedin: req.session.loggedin });
+	if (req.session.loggedin === true) {
+		res.redirect("/");
+		return;
+	}
+	res.render("login", { loggedin: req.session.loggedin });
 });
 
 app.post("/login-action", (req, res) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, (_, fields, __) => {
-        const user = check_credentials(fields.username[0], fields.password[0]);
+	const form = new formidable.IncomingForm();
+	form.parse(req, (_, fields, __) => {
+		const user = check_credentials(fields.username[0], fields.password[0]);
 
-        if (user) {
-            req.session.loggedin = true;
-            res.redirect("/");
-        } else {
-            res.redirect("/login");
-        }
-    });
+		if (user) {
+			req.session.loggedin = true;
+			res.redirect("/");
+		} else {
+			res.redirect("/login");
+		}
+	});
 });
 
 function check_credentials(username, password) {
-    let users = [];
-    if (fs.existsSync("users.json")) {
-        const users_data = fs.readFileSync("users.json");
-        users = JSON.parse(users_data);
-    }
-    for (const user of users) {
-        if (user.username === username && user.password === password) {
-            return true;
-        }
-    }
-    return false;
+	let users = [];
+	if (fs.existsSync("users.json")) {
+		const users_data = fs.readFileSync("users.json");
+		users = JSON.parse(users_data);
+	}
+	for (const user of users) {
+		if (user.username === username && user.password === password) {
+			return true;
+		}
+	}
+	return false;
 }
 
 app.get("/register", (req, res) => {
-    if (req.session.loggedin === true) {
-        res.redirect("/");
-        return;
-    }
+	if (req.session.loggedin === true) {
+		res.redirect("/");
+		return;
+	}
 
-    res.render("register", { loggedin: req.session.loggedin });
+	res.render("register", { loggedin: req.session.loggedin });
 });
 
 app.post("/register-action", (req, res) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, (_, fields, __) => {
-        const added = add_user(fields.username[0], fields.password[0]);
+	const form = new formidable.IncomingForm();
+	form.parse(req, (_, fields, __) => {
+		const added = add_user(fields.username[0], fields.password[0]);
 
-        if (added) {
-            req.session.loggedin = true;
-            res.redirect("/login");
-        } else {
-            res.redirect("/register");
-        }
-    });
+		if (added) {
+			req.session.loggedin = true;
+			res.redirect("/login");
+		} else {
+			res.redirect("/register");
+		}
+	});
 });
 
 function exists_user(users, username) {
-    for (const user in users) {
-        if (user.username === username) {
-            return true;
-        }
-    }
-    return false;
+	for (const user in users) {
+		if (user.username === username) {
+			return true;
+		}
+	}
+	return false;
 }
 
 /// Returns true if a user was added successfully
@@ -102,58 +102,58 @@ function exists_user(users, username) {
 // 2. fs.writeFile succeeded!
 // 3. The password is as specified on the register page
 function add_user(username, password) {
-    let users = [];
-    if (fs.existsSync("users.json")) {
-        const users_data = fs.readFileSync("users.json");
-        users = JSON.parse(users_data);
-    }
+	let users = [];
+	if (fs.existsSync("users.json")) {
+		const users_data = fs.readFileSync("users.json");
+		users = JSON.parse(users_data);
+	}
 
-    if (exists_user(users, username)) {
-        return false;
-    }
+	if (exists_user(users, username)) {
+		return false;
+	}
 
-    const pass_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-    if (!pass_regex.test(password)) {
-        return false;
-    }
+	const pass_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+	if (!pass_regex.test(password)) {
+		return false;
+	}
 
-    users.push({
-        username: username,
-        password: password,
-    });
+	users.push({
+		username: username,
+		password: password,
+	});
 
-    const data = JSON.stringify(users, null, 4);
-    fs.writeFile("users.json", data, (err) => {
-        if (err) {
-            console.log(err);
-            return false;
-        }
-        console.log("File written successfully\n");
-    });
-    return true;
+	const data = JSON.stringify(users, null, 4);
+	fs.writeFile("users.json", data, (err) => {
+		if (err) {
+			console.log(err);
+			return false;
+		}
+		console.log("File written successfully\n");
+	});
+	return true;
 }
 
 app.get("/try", (req, res) => {
-    if (!req.session.loggedin) {
-        res.redirect("/404");
-        return;
-    }
-    res.render("visual", { loggedin: req.session.loggedin });
+	if (!req.session.loggedin) {
+		res.redirect("/404");
+		return;
+	}
+	res.render("visual", { loggedin: req.session.loggedin });
 });
 
 app.get("/logout", (req, res) => {
-    req.session.loggedin = false;
-    res.redirect("/");
+	req.session.loggedin = false;
+	res.redirect("/");
 });
 
 /// Paths that are not handled explicity
 // should go to 404 page
 app.get("*", (req, res) => {
-    res.status(404);
-    res.render("404");
+	res.status(404);
+	res.render("404");
 });
 
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Listening at:  http://localhost:${PORT}`);
 });
