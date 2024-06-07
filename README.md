da43cf98347832c0cf7934672b51abf8ad74b80c
R3Zv <razvan.maracine@protonmail.com>
2024-06-07 22:25:16 +0300

    Login system

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
-# Todo
-- [ ] Make menu its own ejs component
-- [ ] Better menu
-- [ ] Add log out button in menu
-- [ ] Add log in button in menu
-- [ ] Add sign in page
-- [ ] Can go to /try only if you are logged in
+# Pathfinding Visualizer
 
-# Project Goals
+## Usage
+```console
+git clone git@github.com:R3ZV/path-visual.git
 
-## JS
-- [ ] Use localStorage
-- [ ] Use the properties from classList, target or currentTarget
-- [ ] Use getComputedStyle, stopPropagation methods
-- [ ] Validate form data using RegEx (validate the user's email or password)
+cd path-visual
 
-## Node.js
-- [ ] GET/POST requests: using data from a form
-- [ ] Ajax requests by using data from a json file
-- [ ] login/logout system
-- [x] 404 page
+npm install
 
-## Other
-- [ ] Make the website be in 2 different languages
+npm run dev
+```
+
+## Informations
+If you don't see the favicon go to
+http://localhost:8080/favicon.ico
+and it should be fixed.
+
+To try the pathfinding visualizer you have to create an account
+and login.
+
+Or you could use my favorite user 'Json' here are his credentials:
+
+Username: Json
+
+Password: Derulo1
+
+You can hold click to draw walls on the grid.
+
+Clicking a wall will result in removing it.
+
+In the visulizer, based on the cell color the cell has the following meanings:
+- Blue cell = The cell we start from
+- Red cell = The cell we want to reach
+- Black cell = A blocked cell
+- Yellow cell = Processed cell by the algorithm
+- Teal cell = Cell that is part of the shortest path between the red and blue cells
--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const session = require("express-session");
+const favicon = require("serve-favicon");
 const formidable = require("formidable");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(express.static(`${__dirname}/views`));
 app.use(express.static(`${__dirname}/public`));
 app.use(express.static(`${__dirname}/assets`));
 app.use(express.static(`${__dirname}/scripts`));
@@ -18,11 +22,32 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-    res.render("index");
+    if (req.session.loggedin === undefined) {
+        req.session.loggedin = false;
+    }
+    res.render("index", { loggedin: req.session.loggedin });
 });
 
-app.get("/try", (req, res) => {
-    res.render("visual");
+app.get("/login", (req, res) => {
+    if (req.session.loggedin === true) {
+        res.redirect("/");
+        return;
+    }
+    res.render("login", { loggedin: req.session.loggedin });
+});
+
+app.post("/login-action", (req, res) => {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (_, fields, __) => {
+        const user = check_credentials(fields.username[0], fields.password[0]);
+
+        if (user) {
+            req.session.loggedin = true;
+            res.redirect("/");
+        } else {
+            res.redirect("/login");
+        }
+    });
 });
 
 function check_credentials(username, password) {
@@ -39,53 +64,96 @@ function check_credentials(username, password) {
     return false;
 }
 
-app.post("/login", (req, res) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-        user = check_credentials(fields.username, fields.parola);
-        // verificarea datelor de login
+app.get("/register", (req, res) => {
+    if (req.session.loggedin === true) {
+        res.redirect("/");
+        return;
+    }
 
-        if (user) {
-            req.session.username = user;
-            // setez userul ca proprietate a sesiunii
-            res.redirect("/logged");
+    res.render("register", { loggedin: req.session.loggedin });
+});
+
+app.post("/register-action", (req, res) => {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (_, fields, __) => {
+        const added = add_user(fields.username[0], fields.password[0]);
+
+        if (added) {
+            req.session.loggedin = true;
+            res.redirect("/login");
         } else {
-            res.redirect("/");
-            req.session.username = false;
+            res.redirect("/register");
         }
     });
 });
 
-app.get("/logged", (req, res) => {
-    res.render("components/logged", { name: req.session.username });
-});
+function exists_user(users, username) {
+    for (const user in users) {
+        if (user.username === username) {
+            return true;
+        }
+    }
+    return false;
+}
 
-app.post("/login", (req, res) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-        user = check_credentials(fields.username, fields.parola);
-        // verificarea datelor de login
+/// Returns true if a user was added successfully
+// A user is added successfully if:
+// 1. There is no other user with the same username
+// 2. fs.writeFile succeeded!
+// 3. The password is as specified on the register page
+function add_user(username, password) {
+    let users = [];
+    if (fs.existsSync("users.json")) {
+        const users_data = fs.readFileSync("users.json");
+        users = JSON.parse(users_data);
+    }
 
-        if (user) {
-            req.session.username = user;
-            // setez userul ca proprietate a sesiunii
-            res.redirect("/logat");
-        } else req.session.username = false;
+    if (exists_user(users, username)) {
+        return false;
+    }
+
+    const pass_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if (!pass_regex.test(password)) {
+        return false;
+    }
+
+    users.push({
+        username: username,
+        password: password,
     });
-});
 
-app.get("/logat", (req, res) => {
-    res.render("component/logout", { nume: req.session.username });
+    const data = JSON.stringify(users, null, 4);
+    fs.writeFile("users.json", data, (err) => {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+        console.log("File written successfully\n");
+    });
+    return true;
+}
+
+app.get("/try", (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect("/404");
+        return;
+    }
+    res.render("visual", { loggedin: req.session.loggedin });
 });
 
 app.get("/logout", (req, res) => {
-    req.session.destroy();
-    res.render("index");
+    req.session.loggedin = false;
+    res.redirect("/");
 });
 
+/// Paths that are not handled explicity
+// should go to 404 page
 app.get("*", (req, res) => {
     res.status(404);
     res.render("404");
 });
 
-app.listen(8080);
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
